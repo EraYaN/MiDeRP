8c756fded9891e5f62e4f6863a7c43dc413d0a4b
Robin Hes <ninjatuna@outlook.com>
2013-02-14 20:33:42 +0100

    hoping to fix merge conflict

--- pathfinder_robin_erwin/pathfinder_robin_erwin/util.h
+++ pathfinder_robin_erwin/pathfinder_robin_erwin/util.h
@@ -1,2 +1,22 @@
+//Struct initialisations
+struct node {
+	const int x, y; //For debugging
+
+	int visited,
+		previous,
+		distance;
+};
+
+struct vertex {
+	struct node *origin;
+	struct node *destination;
+	
+	int mine;
+};
+
+//Function prototypes
 void *safe_malloc (size_t size);
 node *newNode (int x, int y);
+vertex *newVertexH (int i);
+vertex *newVertexV (int i);
+
