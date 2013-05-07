361cbdf56a016d5671dcf8f266362d002484b7b8
Erwin de Haan <git@erayan.eu>
2013-05-07 16:52:27 +0200

    ExtensionMethods and Versioning.

---
+++ src/controller/Director/ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Director
+{
+    class ExtensionMethods
+    {
+        public static StatusByteCode ToStatusByteCode(this Byte b)
+        {
+            var sbc = (StatusByteCode)b;
+            if (sbc == null)
+            {
+                return ToStatusByteCode.Unknown;
+            }
+            else
+            {
+                return sbc;
+            }
+        }
+    }
+}
