f155c87c9cb5f75a587335c28d0815e376136db2
Robin Hes <ninjatuna@outlook.com>
2013-05-08 17:14:16 +0200

    controller stuff

--- src/controller/Director/Controller.cs
+++ src/controller/Director/Controller.cs
@@ -20,9 +20,19 @@ namespace Director
 		Done = 0x04
 	};
 
+	public enum Direction
+	{
+		Unknown,
+		Left,
+		Right,
+		Up,
+		Down
+	};
+
 	public class Controller
 	{
 		private StatusByteCode _receivedByte;
+		private int _i = 0;
 		
 		public Controller()
 		{
@@ -31,12 +41,50 @@ namespace Director
 
 		private void com_SerialDataEvent(object sender, SerialDataEventArgs e)
 		{
-			uint i = 0;
+			Direction robotDirection; //current direction the robot is pointing in
+			Direction nextAbsoluteDirection; //next direction in terms of the XY grid
+			Direction nextRelativeDirection; //next direction relative to the robot's current one
+			NodeConnection nextNodeConnection;
+
 			_receivedByte = (StatusByteCode)e.DataByte;
+
+			if (_i == 0)
+			{
+				//First run, robot is still outside of field
+
+			}
 
 			if (_receivedByte == StatusByteCode.Enquiry)
 			{
 				//Robot asks for new directions
+				nextNodeConnection = Data.nav.path[_i];
+
+				if ((int)nextNodeConnection.From.X - (int)nextNodeConnection.To.X >= 1)
+				{
+					//Next destination is left of origin
+					nextAbsoluteDirection = Direction.Left;
+				}
+				if ((int)nextNodeConnection.From.X - (int)nextNodeConnection.To.X <= -1)
+				{
+					//Next destination is right of origin
+					nextAbsoluteDirection = Direction.Right;
+				}
+				if ((int)nextNodeConnection.From.Y - (int)nextNodeConnection.To.Y <= -1)
+				{
+					//Next destination is above origin
+					nextAbsoluteDirection = Direction.Up;
+				}
+				if ((int)nextNodeConnection.From.Y - (int)nextNodeConnection.To.Y >= 1)
+				{
+					//Next destination is under origin
+					nextAbsoluteDirection = Direction.Down;
+				}
+				else
+				{
+					//Shit is pretty wrong
+					nextAbsoluteDirection = Direction.Unknown;
+				}
+
 			}
 		}
 	}
