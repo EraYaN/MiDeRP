4f5dc2ab6cd1ee3e19446bf5283276113fbc2a2c
Erwin de Haan <git@erayan.eu>
2013-05-28 23:04:32 +0200

    Fixed automatic source code file creation and add it to bijlagen. Also optimized it for XeLaTeX
    Use XeLaTeX from now on!

--- src/DesignReporter/DesignReporter/Program.cs
+++ src/DesignReporter/DesignReporter/Program.cs
@@ -8,56 +8,70 @@ namespace DesignReporter
 {
     class Program
     {
-        static DirectoryInfo srcPath = new DirectoryInfo("src/");
-        static DirectoryInfo hwPath = new DirectoryInfo("hardware/");
-        static DirectoryInfo docsPath = new DirectoryInfo("docs/");
+        static DirectoryInfo srcPath = new DirectoryInfo("src\\controller");
+        static DirectoryInfo hwPath = new DirectoryInfo("hardware");
+        static DirectoryInfo docsPath = new DirectoryInfo("docs");
         static FileInfo outputPath = new FileInfo("sourceBijlage.tex");
-        static StringBuilder sb = new StringBuilder(
-@"\documentclass{report}
-\input{library/preamble}
-\input{library/style}
-\addbibresource{library/bibliography.bib}
+		static String start = @"\documentclass{report}
+\input{docs/library/preamble}
+\input{docs/library/style}
+\addbibresource{docs/library/bibliography.bib}
 
-\title{EPO-2: Test Code}
+\title{EPO-2: Source code}
 \author{Erwin de Haan \and Others}
 
 
-\begin{document});"); 
+\begin{document}";
         static void Main(string[] args)
         {
             Console.WriteLine("DesignReporter started.");
-
-            FileInfo[] files = srcPath.GetFiles();
-            foreach (FileInfo fi in files)
+			Console.WriteLine(Directory.GetCurrentDirectory());
+            List<FileInfo> files = new List<FileInfo>();
+			files.AddRange(srcPath.GetFiles("*",SearchOption.AllDirectories));
+			files.AddRange(hwPath.GetFiles("*", SearchOption.AllDirectories));
+			files.Sort((x, y) => StringComparer.OrdinalIgnoreCase.Compare(x.FullName, y.FullName));
+			IEnumerable<FileInfo> fileQuery =
+			from fileQ in files
+			where !fileQ.FullName.Contains("obj") && !fileQ.FullName.Contains("Visual Micro") && !fileQ.FullName.Contains("robotsim") && !fileQ.FullName.Contains("Properties") && !fileQ.FullName.Contains("App")
+			select fileQ;
+			
+			StreamWriter file = outputPath.CreateText();
+			file.WriteLine(start);
+			file.WriteLine("\r\n\r\n");
+			foreach (FileInfo fi in fileQuery)
             {
                 string ext = fi.Extension;
                 string caption = fi.Directory.Name + @"/" + fi.Name;
-                string pathescaped = fi.FullName.Replace('\\','/');
+				caption = caption.Replace("_","\\_");
+				string pathescaped = fi.FullName.Replace(outputPath.Directory.FullName+"\\", "").Replace('\\', '/');
                 if (ext == ".cs")
                 {
                     //caption = fi.Directory.Name+@"\"+fi.Name;
-                    sb.AppendLine(String.Format(@"\includecode[csharp]{{1}}{{0}}\newpage", pathescaped, caption));
+                    file.WriteLine(String.Format(@"\includecode[csharp]{{{1}}}{{{0}}}\newpage", pathescaped, caption));
                 }
                 else if (ext == ".c" || ext == ".h")                
                 {
                     //caption = Path.GetDirectoryName(f) + Path.GetFileName(f);
-                    sb.AppendLine(String.Format(@"\includecode[c]{{1}}{{0}}\newpage", pathescaped, caption));
+					file.WriteLine(String.Format(@"\includecode[c]{{{1}}}{{{0}}}\newpage", pathescaped, caption));
                 }
                 else if (ext == ".vhdl" || ext == ".vhd")
                 {
                     //caption = Path.GetDirectoryName(f) + Path.GetFileName(f);
-                    sb.AppendLine(String.Format(@"\includecode[vhdl]{{1}}{{0}}\newpage", pathescaped, caption));
+					file.WriteLine(String.Format(@"\includecode[vhdl]{{{1}}}{{{0}}}\newpage", pathescaped, caption));
                     
                 }
                 else if (ext == ".xaml")
                 {
                     //caption = Path.GetDirectoryName(f) + Path.GetFileName(f);
-                    sb.AppendLine(String.Format(@"\includecode[xaml]{{1}}{{0}}\newpage", pathescaped, caption));
+					file.WriteLine(String.Format(@"\includecode[xaml]{{{1}}}{{{0}}}\newpage", pathescaped, caption));
 
                 }
             }
-            sb.Append(@"\r\n\end{document}");
-            outputPath.CreateText().WriteLine(sb.ToString());            
+            file.WriteLine(@"\end{document}");
+            file.Close();
+			Console.WriteLine("Done. Press any key to exit.");
+			Console.ReadKey();
         }
+
     }
 }
